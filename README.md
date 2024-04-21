a2ba46a638f5dbaad677d267afb7ec001879bd5d
Mateusz Duda <mateusz.duda2@gmail.com>
2024-04-21 17:25:11 +0200

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Shader showcase
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/8cbb007d-7fdf-4cba-b659-b6e59f0c0f6b/deploy-status)](https://duda.run/)
+[![Netlify Status](https://api.netlify.com/api/v1/badges/d1956517-5ec6-414c-a859-38464adcc55a/deploy-status)](https://app.netlify.com/sites/matiduda/deploys)
 
 A simple website with WebGL shader showcase made with [shader-doodle](https://github.com/halvves/shader-doodle) and [Shadertoy API](https://www.shadertoy.com/howto).
--- script/main.js
+++ script/main.js
@@ -125,3 +125,75 @@ getUserShadersIds("tanczmy", API_KEY).then(fetchedIds => {
   nextDoodle();
   setInterval(nextDoodle, SHADER_DISPLAY_TIME);
 });
+
+// Tabs
+
+let arrowShowTimeoutId = null;
+
+function openTab(event, tabName) {
+  const currentButton = event.target;
+
+  hideArrow();
+
+  let clickedOnActiveTab = false;
+
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    // Also hide the parent
+    tabcontent[i].parentElement.style.opacity = 0;
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    if (tablinks[i].style.backgroundColor !== "" && tablinks[i].id === currentButton.id) {
+      clickedOnActiveTab = true;
+    }
+
+    tablinks[i].style.backgroundColor = "";
+    tablinks[i].style.color = "#ffffff";
+  }
+
+
+  // Show the specific tab content
+  const tab = document.getElementById(tabName);
+  tab.style.display = "block";
+
+  if (clickedOnActiveTab) {
+    arrowShowTimeoutId = showArrowAfterTimeout();
+    return;
+  }
+
+  tab.parentElement.style.opacity = 100;
+
+  // Add the specific color to the button used to open the tab content
+  currentButton.style.backgroundColor = "#ffffffdd";
+  currentButton.style.color = "#000000";
+}
+
+document.querySelector("#aboutmeButton").addEventListener("click", (event) => openTab(event, 'aboutme'))
+document.querySelector("#projectsButton").addEventListener("click", (event) => openTab(event, 'projects'))
+document.querySelector("#contactButton").addEventListener("click", (event) => openTab(event, 'contact'))
+
+function hideArrow() {
+  if (arrowShowTimeoutId) {
+    clearTimeout(arrowShowTimeoutId);
+    arrowShowTimeoutId = null;
+    return;
+  }
+  const arrowElement = document.querySelector("#arrow");
+  arrowElement.style.opacity = 0;
+}
+
+function showArrow() {
+  const arrowElement = document.querySelector("#arrow");
+  arrowElement.style.opacity = 100;
+  arrowShowTimeoutId = null;
+}
+
+function showArrowAfterTimeout() {
+  return setTimeout(showArrow, 3_000);
+}
